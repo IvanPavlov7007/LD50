5dc18014702f90f785ecd7f8b6cc18e80f8729ae
IvanPavlov <ivanpavlov7007@gmail.com>
2022-04-02 18:17:30 +0200

    Create walking prototype
    SampleScene0

---
+++ Assets/Scripts/Objects/CharacterMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed, runningSpeed;
+    public bool isRunning;
+
+    protected Rigidbody rb;
+    protected Animator anim;
+
+    
+    public Vector3 direction { get; set; }
+
+
+    protected void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+    protected void Update()
+    {
+        anim.SetBool("walk", direction.magnitude > 0.01f);
+    }
+
+    protected private void FixedUpdate()
+    {
+        var velocity = direction * (isRunning ? runningSpeed : speed); 
+        rb.velocity = CommonTools.yPlaneVector(velocity, rb.velocity.y);
+    }
+}
---
+++ Assets/Scripts/Ship/Door/DoorController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    Transform body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = transform.Find("Body");
+    }
+
+    public void Open()
+    {
+        body.gameObject.SetActive(false);
+    }
+
+    public void Close()
+    {
+        body.gameObject.SetActive(true);
+    }
+
+
+}
