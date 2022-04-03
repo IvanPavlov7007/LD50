6e7fd8257fff87c1f243bcdd2e39f989b800da16
Robert <github@kosro.de>
2022-04-04 00:20:52 +0200

    ADD: Smooth camera movement

--- Assets/Scripts/Objects/CharacterMovement.cs
+++ Assets/Scripts/Objects/CharacterMovement.cs
@@ -12,7 +12,7 @@ public class CharacterMovement : MonoBehaviour
     protected Animator anim;
     protected SpriteRenderer sr;
 
-
+    public bool facingRight;
 
     public Vector3 direction { get; set; }
 
@@ -26,13 +26,14 @@ public class CharacterMovement : MonoBehaviour
     protected void Update()
     {
         anim.SetBool("walk", direction.magnitude > 0.001f);
-        if(direction.magnitude > 0.001f)
-        sr.flipX = Vector3.Angle(direction, Vector3.right) > 90f;
+        if (direction.magnitude > 0.001f)
+            facingRight = !(Vector3.Angle(direction, Vector3.right) > 90f);
+        sr.flipX = !facingRight;
     }
 
     protected private void FixedUpdate()
     {
-        var velocity = direction * (isRunning ? runningSpeed : speed); 
+        var velocity = direction * (isRunning ? runningSpeed : speed);
         rb.velocity = CommonTools.yPlaneVector(velocity, rb.velocity.y);
     }
 }
