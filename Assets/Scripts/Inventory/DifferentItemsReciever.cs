d42e245030e6eac1aea9c595a93cb62fb285f743
IvanPavlov <ivanpavlov7007@gmail.com>
2022-04-04 21:34:23 +0200

    Add:medic dialogs

---
+++ Assets/Scripts/Inventory/DifferentItemsReciever.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DifferentItemsReciever : ItemReciever
+{
+    public List<ItemAndAction> itemsAndActinos;
+    public override bool Drop(Item item)
+    {
+        var i = itemsAndActinos.Find(x => x.item == item);
+        if (i.item != null && item == i.item)
+        {
+            i.action.Invoke();
+            return i.itemToBeRecieved;
+        }
+        return false;
+    }
+}
+
+[System.Serializable]
+public struct ItemAndAction
+{
+    public Item item;
+    public UnityEvent action;
+    public bool itemToBeRecieved;
+}
--- Assets/Scripts/Inventory/SpecificItemReciever.cs
+++ Assets/Scripts/Inventory/SpecificItemReciever.cs
@@ -18,3 +18,5 @@ public class SpecificItemReciever : ItemReciever
         return false;
     }
 }
+
+
