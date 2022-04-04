c4208b19e2765f58903e0e9a33a1720615589ee1
Robert <github@kosro.de>
2022-04-04 15:48:04 +0200

    ADD: Main Menu music & basic visuals

---
+++ Assets/AudioDelayedStart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioDelayedStart : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public float delay = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.PlayDelayed(delay);
+    }
+}
